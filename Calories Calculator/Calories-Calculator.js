c9653afc3cffe0f56e006f817b2e08f8d6ba40c2
kristopher <kristopher.takken@live.com.au>
2020-07-12 19:40:47 +0930

    Changes

--- Calories Calculator/Calories-Calculator.js
+++ Calories Calculator/Calories-Calculator.js
@@ -79,7 +79,7 @@ function itemDataToPage(){
     gms = itemsToAdd[i][2]
     totCal = (cal/gms) * 100
     document.getElementById('itemData').innerHTML +=`
-    <div class="item" style="width:700px;">
+    <div class="item" style="width:320px;">
       <h4><b>Food:</b> ${itemsToAdd[i][0]}</h4>
       <h4><b>Calories:</b> ${totCal.toFixed(0)} per 100g</h4>
     </div>
