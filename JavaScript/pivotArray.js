13aa44dce7aaecc3c211dc771340f1afdd7b3004
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2020-12-06 15:05:56 +1030

    Create pivotArray.js

---
+++ JavaScript/pivotArray.js
@@ -0,0 +1,36 @@
+function getPivotArray(dataArray, rowIndex, colIndex, dataIndex) {
+            //Code from https://techbrij.com
+			var result = {}, ret = [];
+            var newCols = [];
+            for (var i = 0; i < dataArray.length; i++) {
+
+                if (!result[dataArray[i][rowIndex]]) {
+                    result[dataArray[i][rowIndex]] = {};
+                }
+                result[dataArray[i][rowIndex]][dataArray[i][colIndex]] = dataArray[i][dataIndex];
+
+                //To get column names
+                if (newCols.indexOf(dataArray[i][colIndex]) == -1) {
+                    newCols.push(dataArray[i][colIndex]);
+                }
+            }
+
+            newCols.sort();
+            var item = [];
+
+            //Add Header Row
+            item.push('Item');
+            item.push.apply(item, newCols);
+            ret.push(item);
+
+            //Add content 
+            for (var key in result) {
+                item = [];
+                item.push(key);
+                for (var i = 0; i < newCols.length; i++) {
+                    item.push(result[key][newCols[i]] || "-");
+                }
+                ret.push(item);
+            }
+            return ret;
+        }
