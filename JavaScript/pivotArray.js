8906b2a2e76e06be57fecf630a75ae0d22684c12
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2020-12-06 15:08:16 +1030

    Update pivotArray.js

--- JavaScript/pivotArray.js
+++ JavaScript/pivotArray.js
@@ -34,3 +34,17 @@ function getPivotArray(dataArray, rowIndex, colIndex, dataIndex) {
             }
             return ret;
         }
+
+function arrayToHTMLTable(myArray) {
+            var result = "<table border='1' cellpadding='7' cellspacing='0'>";
+            for (var i = 0; i < myArray.length; i++) {
+                result += "<tr>";
+                for (var j = 0; j < myArray[i].length; j++) {
+                    result += "<td>" + myArray[i][j] + "</td>";
+                }
+                result += "</tr>";
+            }
+            result += "</table>";
+
+            return result;
+        }
