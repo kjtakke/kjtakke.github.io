f0ece32dc58b7d122245c245b23725f5ac4d550c
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2020-12-08 22:14:29 +1030

    Update csv-array-functions.js

--- JavaScript/csv-array-functions.js
+++ JavaScript/csv-array-functions.js
@@ -49,23 +49,22 @@ function arrayToHTMLTable(myArray) {
         }
 
 function csvToArray(csv_url){
+  var csvAray = []
   $.ajax({
-   url:csv_url,
-   dataType:text,
-   success:function(data)
-   {
-    var employee_data = data.split(/\r?\n|\r/);
-
-    //Array
-    for(var i = 0; i < employee_data.length; i++){
-      let tempArray = employee_data[i].split(",")
-      csvAray.push(tempArray)
+    url:csv_url,
+    dataType:"text",
+    success:function(data) {
+      var csvData = data.split(/\r?\n|\r/)
+      for(var i = 0; i < csvData.length; i++){
+        let tempArray = csvData[i].split(",")
+        csvAray.push(tempArray)
+      }
+      //document.getElementById("txt").innerHTML = csvAray[0][0]
     }
-    document.getElementById("txt").innerHTML = csvAray[0][0];
-  }
-});
-return csvAray
-};
+  })
+  return csvAray
+}
+
 
 function arrayToDictionary(arr) {
   
